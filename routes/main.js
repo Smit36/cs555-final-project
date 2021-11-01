fb6ccd1f7dd04f17f968f344b875953102e3df0a
Gavin Pan <55461412+gpan1@users.noreply.github.com>
2021-10-31 20:25:55 -0400

    Basic Signup Routes and Error checking created, added basic xss and bcrypt security
    Signup database still needs to be created, otherwise user is able to signup, but it does not go into the database. Will pair program with tyler for that. User data is protected with bcrypt and site is secured with xss

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 16;
+const xss = require("xss");
+const usersDatabase = require("../data/users")
 
 // This route will render our home page. If it cannot, it will send a 500 error and render the error.
 router.get("/", async (req, res) => {
@@ -9,4 +13,46 @@ router.get("/", async (req, res) => {
     res.status(500).render("errors/error", { error: e });
   }
 });
+
+router.get('/signup', async (req,res) => { 
+  return res.render("signup.handlebars", {title: "Sign up"});
+});
+
+router.post('/signup', async (req,res) => {
+  let {email, username, password, confirmpassword} = req.body;
+  email = xss(email);
+  username=xss(username);
+  password=xss(password);
+  confirmpassword=xss(confirmpassword);
+  
+  if (!email || typeof(email) !== 'string' || email.trim().length == 0) {
+      return res.status(400).render("signup.handlebars", {title: "Sign up failed", errormsg: "Error: Email not provided or is not a valid string."});
+  }
+  try {
+      errorChecker.ValidateEmail(email);
+  } catch(e) {
+      return res.status(400).render("signup.handlebars", {title: "Sign up failed", errormsg: "Error: Please enter a valid E-mail."});
+  }
+  if (!username || typeof(username) !== "string" || username.trim().length == 0) {
+      return res.status(400).render("signup.handlebars", {title: "Sign up failed", errormsg: "Error: Username not provided or is not a valid string."});
+  }
+  if (!password || typeof(password) !== "string" || password.trim().length == 0) {
+      return res.status(400).render("signup.handlebars", {title: "Sign up failed", errormsg: "Error: Password not provided or is not a valid string."});
+  }
+  if (!confirmpassword || typeof(confirmpassword) !== "string" || confirmpassword.trim().length == 0) {
+      return res.status(400).render("signup.handlebars", {title: "Sign up failed", errormsg: "Error: Confirm password not provided or is not a valid string."});
+  }
+  if (password !== confirmpassword) {
+      return res.status(400).render("signup.handlebars", {title: "Sign up failed", errormsg: "Error: Passwords do not match."});
+  }
+  const realusername = username.toLowerCase();
+  const hashPassword = await bcrypt.hash(password, saltRounds);
+  try {
+      await usersDatabase.createUser("user", realusername, hashPassword, email);
+  } catch (e){
+      return res.status(500).render("signupError.handlebars", {title: "Sign up failed", error: e});
+  }
+  return res.render("signupSuccess.handlebars", {title: "Sign up success", user: realusername}); 
+});
+
 module.exports = router;
